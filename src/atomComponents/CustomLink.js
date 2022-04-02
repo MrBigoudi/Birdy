8abc9980d360e430e340093913eae18788f32213
MrBigoudi <yannis.kedadry2@gmail.com>
2022-04-02 10:31:45 +0200

    add server dir

--- src/atomComponents/CustomLink.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function CustomLink(props){
-    return (
-        <a className="default-font link-no-style link-blue" target="_blanck" href={props.url}>{props.msg}</a>
-    )
-}
--- src/constants/errorMessages.js
+++
@@ -1,4 +0,0 @@
-export const WRONG_EMAIL = "Sorry, this email address doesn't belong to an account.";
-export const WRONG_PASSWD = "Wrong password.";
-export const EMAIL_TAKEN = "An account is already assigned to this email address.";
-export const USERNAME_TAKEN = "This username is taken, please chose another one.";
